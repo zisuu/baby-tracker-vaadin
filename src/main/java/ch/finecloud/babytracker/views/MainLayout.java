9c9f6265968143e427e5674abe99905ad7986e61
zisuu <david.ziswiler@finecloud.ch>
2023-10-03 22:43:09 +0200

    feat: improve menu with icons

--- src/main/java/ch/finecloud/babytracker/views/MainLayout.java
+++ src/main/java/ch/finecloud/babytracker/views/MainLayout.java
@@ -8,9 +8,14 @@ import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.lumo.LumoUtility;
@@ -47,11 +52,31 @@ public class MainLayout extends AppLayout {
     }
 
     private void createDrawer() {
-        addToDrawer(new VerticalLayout(
-                new RouterLink("My Babies", BabyListView.class),
-                new RouterLink("My Events", EventListView.class)
-//                new RouterLink("Dashboard", DashboardView.class)
-        ));
+        Tabs tabs = getTabs();
+        addToDrawer(tabs);
+    }
+
+    private Tabs getTabs() {
+        Tabs tabs = new Tabs();
+        tabs.add(createTab(VaadinIcon.USERS, "My Babies", BabyListView.class),
+                createTab(VaadinIcon.LIST, "My Events", EventListView.class));
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        return tabs;
+    }
+
+    private Tab createTab(VaadinIcon viewIcon, String viewName, Class viewClass) {
+        Icon icon = viewIcon.create();
+        icon.getStyle().set("box-sizing", "border-box")
+                .set("margin-inline-end", "var(--lumo-space-m)")
+                .set("margin-inline-start", "var(--lumo-space-xs)")
+                .set("padding", "var(--lumo-space-xs)");
+
+        RouterLink link = new RouterLink();
+        link.add(icon, new Span(viewName));
+        link.setRoute(viewClass);
+        link.setTabIndex(-1);
+
+        return new Tab(link);
     }
 
 

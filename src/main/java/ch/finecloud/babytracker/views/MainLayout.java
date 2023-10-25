accb8f320f8e31e5f0eb99df8482a547226d6913
zisuu <david.ziswiler@finecloud.ch>
2023-10-25 20:38:49 +0200

    fix: menu selection

--- src/main/java/ch/finecloud/babytracker/views/MainLayout.java
+++ src/main/java/ch/finecloud/babytracker/views/MainLayout.java
@@ -58,8 +58,8 @@ public class MainLayout extends AppLayout {
 
     private Tabs getTabs() {
         Tabs tabs = new Tabs();
-        tabs.add(createTab(VaadinIcon.USERS, "My Babies", BabyListView.class),
-                createTab(VaadinIcon.LIST, "My Events", EventListView.class));
+        tabs.add(createTab(VaadinIcon.LIST, "My Events", EventListView.class),
+                createTab(VaadinIcon.USERS, "My Babies", BabyListView.class));
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         return tabs;
     }

b1e48dd7f3ae0c0acbce68c755581a93741895a4
zisuu <david.ziswiler@finecloud.ch>
2023-12-04 15:32:17 +0100

    refactor: update banner

--- src/test/java/ch/finecloud/babytracker/BabyTrackerApplicationTests.java
+++ src/test/java/ch/finecloud/babytracker/BabyTrackerApplicationTests.java
@@ -1,13 +1,13 @@
-package ch.finecloud.babytracker;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BabyTrackerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package ch.finecloud.babytracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BabyTrackerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
